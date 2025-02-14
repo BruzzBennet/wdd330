e2f24a03a7b97fad68d0fefe9a21235407e6e288
BruzzBennet <bruce.bennet.trevino@gmail.com>
2025-02-13 19:50:08 -0600

    icon

--- fakemon/scripts/pin.js
+++ fakemon/scripts/pin.js
@@ -4,7 +4,7 @@ const pin3 = document.querySelector('#pin3');
 const pin4 = document.querySelector('#pin4');
 
 function pin(pinner){
-    pinner.addEventList('click', () => {
+    pinner.addEventListener('click', () => {
         pinner.classList.toggle('clicked');
     });
 }
